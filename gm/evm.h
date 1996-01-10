5ca71e45962f34817d2c1c05d51215061b25641b
gabi <gabi>
1996-01-10 16:17:33 +0000

    Erweiterung der typedefs of DIM-routines
    [[Imported from SVN: r181]]

--- gm/evm.h
+++ gm/evm.h
@@ -215,50 +215,47 @@
 
 #ifdef __TWODIM__
 
-#define V_DIM_LINCOMB(a,A,b,B,C)		   	V2_LINCOMB(a,A,b,B,C)
-#define V_DIM_COPY(A,C)				   		V2_COPY(A,C)
-#define V_DIM_SUBTRACT(A,B,C)			   	V2_SUBTRACT(A,B,C)
-#define V_DIM_ADD(A,B,C)				   	V2_ADD(A,B,C)
-#define V_DIM_SCALE(c,C)				   	V2_SCALE(c,C)
-#define V_DIM_VECTOR_PRODUCT(A,B,c)			V2_VECTOR_PRODUCT(A,B,c)
-#define V_DIM_COMPARE(A,B,c)				V2_COMPARE(A,B,c)
-#define V_DIM_ISEQUAL(A,B) 					V2_ISEQUAL(A,B)
-#define V_DIM_EUKLIDNORM(A,b)				V2_EUKLIDNORM(A,b)
+#define V_DIM_LINCOMB(a,A,b,B,C)		V2_LINCOMB(a,A,b,B,C)
+#define V_DIM_COPY(A,C)				V2_COPY(A,C)
+#define V_DIM_SUBTRACT(A,B,C)			V2_SUBTRACT(A,B,C)
+#define V_DIM_ADD(A,B,C)			V2_ADD(A,B,C)
+#define V_DIM_SCALE(c,C)			V2_SCALE(c,C)
+#define V_DIM_VECTOR_PRODUCT(A,B,c)		V2_VECTOR_PRODUCT(A,B,c)
+#define V_DIM_COMPARE(A,B,c)			V2_COMPARE(A,B,c)
+#define V_DIM_ISEQUAL(A,B) 			V2_ISEQUAL(A,B)
+#define V_DIM_EUKLIDNORM(A,b)			V2_EUKLIDNORM(A,b)
 #define V_DIM_EUKLIDNORM_OF_DIFF(A,B,b)		V2_EUKLIDNORM_OF_DIFF(A,B,b)
-#define V_DIM_CLEAR(A) 						V2_CLEAR(A)
-#define V_DIM_SCALAR_PRODUCT(A,B,c)			V2_SCALAR_PRODUCT(A,B,c)
-#define V_DIM_ISZERO(A)						V2_ISZERO(A)
-#define M_TIMES_V_DIM(M,A,B)	            {(B)[0] = (M)[0][0]*(A)[0]    \
-											   + (M)[0][1]*(A)[1];        \
-										     (B)[1] = (M)[1][0]*(A)[0]    \
-											   + (M)[1][1]*(A)[1];}
+#define V_DIM_CLEAR(A) 				V2_CLEAR(A)
+#define V_DIM_SCALAR_PRODUCT(A,B,c)		V2_SCALAR_PRODUCT(A,B,c)
+#define V_DIM_ISZERO(A)				V2_ISZERO(A)
+#define M_TIMES_V_DIM(M,A,B)	             	M2_TIMES_V2(M,A,B)
+#define M_DIM_ADD(A,B,C)			M2_ADD(A,B,C)
+#define M_DIM_COPY(A,C)				M2_COPY(A,C)
+#define M_DIM_SCALE(c,M)			M2_SCALE(c,M)
+#define V_DIM_Normalize(a)			V2_Normalize(a)
 
 #endif
 
 #ifdef __THREEDIM__
 
-#define V_DIM_LINCOMB(a,A,b,B,C)		   	V3_LINCOMB(a,A,b,B,C)
-#define V_DIM_COPY(A,C)				   		V3_COPY(A,C)
-#define V_DIM_SUBTRACT(A,B,C)			   	V3_SUBTRACT(A,B,C)
-#define V_DIM_ADD(A,B,C)				   	V3_ADD(A,B,C)
-#define V_DIM_SCALE(c,C)				   	V3_SCALE(c,C)
-#define V_DIM_VECTOR_PRODUCT(A,B,c)			V3_VECTOR_PRODUCT(A,B,c)
-#define V_DIM_COMPARE(A,B,c)				V3_COMPARE(A,B,c)
-#define V_DIM_ISEQUAL(A,B) 					V3_ISEQUAL(A,B)
-#define V_DIM_EUKLIDNORM(A,b)				V3_EUKLIDNORM(A,b)
+#define V_DIM_LINCOMB(a,A,b,B,C)		V3_LINCOMB(a,A,b,B,C)
+#define V_DIM_COPY(A,C)				V3_COPY(A,C)
+#define V_DIM_SUBTRACT(A,B,C)			V3_SUBTRACT(A,B,C)
+#define V_DIM_ADD(A,B,C)			V3_ADD(A,B,C)
+#define V_DIM_SCALE(c,C)			V3_SCALE(c,C)
+#define V_DIM_VECTOR_PRODUCT(A,B,c)		V3_VECTOR_PRODUCT(A,B,c)
+#define V_DIM_COMPARE(A,B,c)			V3_COMPARE(A,B,c)
+#define V_DIM_ISEQUAL(A,B) 			V3_ISEQUAL(A,B)
+#define V_DIM_EUKLIDNORM(A,b)			V3_EUKLIDNORM(A,b)
 #define V_DIM_EUKLIDNORM_OF_DIFF(A,B,b)		V3_EUKLIDNORM_OF_DIFF(A,B,b)
-#define V_DIM_CLEAR(A) 						V3_CLEAR(A)
-#define V_DIM_SCALAR_PRODUCT(A,B,c)			V3_SCALAR_PRODUCT(A,B,c)
-#define V_DIM_ISZERO(A)						V3_ISZERO(A)
-#define M_TIMES_V_DIM(M,A,B)			    {(B)[0] = (M)[0][0]*(A)[0]    \
-											   + (M)[0][1]*(A)[1]         \
-											   + (M)[0][2]*(A)[2];        \
-										     (B)[1] = (M)[1][0]*(A)[0]    \
-											   + (M)[1][1]*(A)[1]         \
-											   + (M)[1][2]*(A)[2];        \
-											 (B)[2] = (M)[2][0]*(A)[0]    \
-											   + (M)[2][1]*(A)[1]         \
-											   + (M)[2][2]*(A)[2];}
+#define V_DIM_CLEAR(A) 				V3_CLEAR(A)
+#define V_DIM_SCALAR_PRODUCT(A,B,c)		V3_SCALAR_PRODUCT(A,B,c)
+#define V_DIM_ISZERO(A)				V3_ISZERO(A)
+#define M_TIMES_V_DIM(M,A,B)			M3_TIMES_V3(M,A,B)
+#define M_DIM_ADD(A,B,C) 		   	M3_ADDMATRIX(A,B,C)
+#define M_DIM_COPY(A,C)				M3_COPY(A,C)
+#define M_DIM_SCALE(c,M)			M3_SCALE(c,M)
+#define V_DIM_Normalize(a)			V3_Normalize(a)
 
 #endif
 
