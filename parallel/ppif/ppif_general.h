68cc89ed3752b15b9a263889faa4a049106d55b9
Stefan Lang <stefan@dune-project.org>
1996-06-24 16:15:11 +0000

    RCS keyword string for ident needs space before closing '$' character!!!
    [[Imported from SVN: r629]]

--- parallel/ppif/ppif_general.h
+++ parallel/ppif/ppif_general.h
@@ -45,7 +45,7 @@
 #define PPIFRCSSTRING(ppif_version,arch_version)\
   PPIFRCSSTRINGAUX(ppif_version,arch_version)
 #define PPIFRCSSTRINGAUX(ppif_version,arch_version)\
-  "$" "State: PPIF_VERSION=" # ppif_version " ARCH_VERSION=" # arch_version "$"
+  "$" "State: PPIF_VERSION="ppif_version " ARCH_VERSION="arch_version " $"
 
 #define RCSID(header,module_rcs_string) RCSIDAUX(header,module_rcs_string)
 #define RCSIDAUX(header,module_rcs_string) static char rcsid[] = header ## module_rcs_string;
