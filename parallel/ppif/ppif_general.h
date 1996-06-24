66569233e8fa87f376d24a887d7a5f37c469138e
Stefan Lang <stefan@dune-project.org>
1996-06-24 10:48:56 +0000

    header file included in all ppif.c files
    [[Imported from SVN: r621]]

---
+++ parallel/ppif/ppif_general.h
@@ -0,0 +1,76 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+/****************************************************************************/
+/*																			*/
+/* File:	  ppif_general.h												*/
+/*																			*/
+/* Purpose:   ppif general header file                                                                  */
+/*																			*/
+/* Author:	  Stefan Lang                                                                           */
+/*			  Institut fuer Computeranwendungen III                                                 */
+/*			  Universitaet Stuttgart										*/
+/*			  Pfaffenwaldring 27											*/
+/*			  70550 Stuttgart												*/
+/*			  email: ug@ica3.uni-stuttgart.de								*/
+/*																			*/
+/* History:   24.06.96 start: PPIF_VERSION string, RCSID macro              */
+/*																			*/
+/* Remarks:                                                                                                                             */
+/*																			*/
+/****************************************************************************/
+
+/****************************************************************************/
+/*																			*/
+/* auto include mechanism and other include files							*/
+/*																			*/
+/****************************************************************************/
+
+#ifndef __GENREAL__
+#define __GENERAL__
+
+/****************************************************************************/
+/*																			*/
+/* defines in the following order											*/
+/*																			*/
+/*		  compile time constants defining static data size (i.e. arrays)	*/
+/*		  other constants													*/
+/*		  macros															*/
+/*																			*/
+/****************************************************************************/
+
+/* defintions of RCS macros */
+#define PPIF_VERSION "PPIF_1_0"
+
+#define PPIF_RCS_STRING PPIFRCSSTRING(PPIF_VERSION)
+#define PPIFRCSSTRING(ppif_version)\
+  PPIFRCSSTRINGAUX(ppif_version)
+#define PPIFRCSSTRINGAUX(ppif_version)\
+  "$" "State: PPIF_VERSION=" # ppif_version "$"
+
+#define RCSID(header,module_rcs_string) RCSIDAUX(header,module_rcs_string)
+#define RCSIDAUX(header,module_rcs_string) static char rcsid[] = # header ## module_rcs_string;
+
+
+/****************************************************************************/
+/*																			*/
+/* data structures exported by the corresponding source file				*/
+/*																			*/
+/****************************************************************************/
+
+
+
+/****************************************************************************/
+/*																			*/
+/* definition of exported global variables									*/
+/*																			*/
+/****************************************************************************/
+
+
+
+/****************************************************************************/
+/*																			*/
+/* function declarations													*/
+/*																			*/
+/****************************************************************************/
+
+#endif
