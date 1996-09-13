3ea631b3564ce33b5e8369a52697b7d1daefd0f3
wieners <wieners>
1996-09-13 13:30:27 +0000

    Hier ist es, das neue domain!
    Wichtigste Neuerung: es gibt keine Patches mehr
    und die Aufrufe fuer die Randbedingungsauswertung hat sich geaendert.
    
    ug besitzt nun einige vordefiniert Gebiete, z. B. Torus oder
    Wolfgangsee. weitere Beipiele erwuenscht!
    
    [[Imported from SVN: r981]]

--- dom/domain.h
+++ dom/domain.h
@@ -1,28 +1,28 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
 /****************************************************************************/
-/*																			*/
-/* File:	  domain.h														*/
-/*																			*/
-/* Purpose:   interface to domain description                               */
-/*																			*/
-/* Author:	  Peter Bastian/Klaus Johannsen                                                                 */
-/*			  Institut fuer Computeranwendungen III                                                 */
-/*			  Universitaet Stuttgart										*/
-/*			  Pfaffenwaldring 27											*/
-/*			  70550 Stuttgart												*/
-/*			  email: ug@ica3.uni-stuttgart.de								*/
-/*																			*/
-/* History:   29.01.92 begin, ug version 2.0								*/
-/*																			*/
-/* Remarks:                                                                                                                             */
-/*																			*/
+/*                                                                          */
+/* File:      domain.h                                                      */
+/*                                                                          */
+/* Purpose:   standard header file template                                 */
+/*                                                                          */
+/* Author:      klaus Johannsen                                             */
+/*              Institut fuer Computeranwendungen III                       */
+/*              Universitaet Stuttgart                                      */
+/*              Pfaffenwaldring 27                                          */
+/*              70550 Stuttgart                                             */
+/*              email: ug@ica3.uni-stuttgart.de                             */
+/*                                                                          */
+/* History:   18.06.96 begin, ug version 3.2                                */
+/*                                                                          */
+/* Remarks:                                                                 */
+/*                                                                          */
 /****************************************************************************/
 
 /****************************************************************************/
-/*																			*/
-/* auto include mechanism and other include files							*/
-/*																			*/
+/*                                                                          */
+/* auto include mechanism and other include files                           */
+/*                                                                          */
 /****************************************************************************/
 
 #ifndef __DOMAIN__
@@ -36,164 +36,179 @@
 #include "ugenv.h"
 #endif
 
+#ifndef __HEAPS__
+#include "heaps.h"
+#endif
+
 #ifdef __MWCW__
 #include "MWCW.cmdlinedefs"
 #endif
 
 /****************************************************************************/
-/*																			*/
-/* defines in the following order											*/
-/*																			*/
-/*		  compile time constants defining static data size (i.e. arrays)	*/
-/*		  other constants													*/
-/*		  macros															*/
-/*																			*/
+/*                                                                          */
+/* defines in the following order                                           */
+/*                                                                          */
+/*          compile time constants defining static data size (i.e. arrays)  */
+/*          other constants                                                 */
+/*          macros                                                          */
+/*                                                                          */
 /****************************************************************************/
 
 #ifdef _2
-        #ifdef _3
-        #error ****	define EITHER dimension _2 OR _3	   ****
-        #endif
+#ifdef _3
+#error ****    define EITHER dimension _2 OR _3       ****
+#endif
 #define __TWODIM__
 #define DIM 2
 #define DIM_OF_BND 1
-#define CORNERS_OF_BND_SEG      2
 #endif
 
 #ifdef _3
 #define __THREEDIM__
 #define DIM 3
 #define DIM_OF_BND 2
-#define CORNERS_OF_BND_SEG 4
 #endif
 
 #ifndef _2
 #ifndef _3
-#error ****	define at least dimension two OR three		****
+#error ****    define at least dimension two OR three        ****
 #endif
 #endif
 
-/* compile time constants */
-#define BVPD_MAX_COEFFFCTS              10
-#define BVPD_MAX_USERFCTS               10
+/* object identification */
+#define BSOBJ 8                                                 /* boundary surface                 */
+#define BPOBJ 9                                                 /* boundary point object			*/
 
-/* boundary segment types */
-#define PERIODIC                                1
-#define NON_PERIODIC                    2
-#define FREE                                    3
+/* boundary types */
+#define FIXED         0
+#define FREE          1
+#define PERIODIC      2
+#define NON_PERIODIC  3
 
-/* some function formats */
+/* function formats */
 typedef INT (*ConfigProcPtr)(INT argc, char **argv);
 typedef INT (*CoeffProcPtr)(COORD *, DOUBLE *);
 typedef INT (*UserProcPtr)(DOUBLE *, DOUBLE *);
 
 /* macros for BVPDescriptor */
-#define BVPD_NAME(d)                    ((d).name)
-#define BVPD_MIDPOINT(d)                ((d).midpoint)
-#define BVPD_RADIUS(d)                  ((d).radius)
-#define BVPD_NCORNERS(d)                ((d).nCorners)
-#define BVPD_CONVEX(d)                  ((d).convex)
-#define BVPD_NPATCHES(d)                ((d).nPatches)
-#define BVPD_NSUBDOM(d)                 ((d).nSubDomains)
-#define BVPD_ID(d)                          ((d).id)
-#define BVPD_CONFIG(d)                  ((d).ConfigProc)
-#define BVPD_NCOEFFF(d)                 ((d).numOfCoeffFct)
-#define BVPD_NUSERF(d)                  ((d).numOfUserFct)
-
-/* macros for PatchDescriptor */
-#define PATCH_LEFT(p)                   ((p).left)
-#define PATCH_RIGHT(p)                  ((p).right)
-#define PATCH_TYPE(p)                   ((p).type)
-#define PATCH_N(p)                              ((p).n)
-#define PATCH_ID(p)                             ((p).id)
-#define PATCH_RES(p)                    ((p).resolution)
-#define PATCH_CID(p,i)                  ((p).pc[i].id)
-#define PATCH_LCVECT(p,i)               ((p).pc[i].lcoord)
+#define BVPD_NAME(d)         ((d).name)
+#define BVPD_MIDPOINT(d)     ((d).midpoint)
+#define BVPD_RADIUS(d)       ((d).radius)
+#define BVPD_CONVEX(d)       ((d).convex)
+#define BVPD_NSUBDOM(d)      ((d).nSubDomains)
+#define BVPD_CONFIG(d)       ((d).ConfigProc)
+#define BVPD_NCOEFFF(d)      ((d).numOfCoeffFct)
+#define BVPD_NUSERF(d)       ((d).numOfUserFct)
 
 /****************************************************************************/
-/*																			*/
-/* data structures exported by the corresponding source file				*/
-/*																			*/
+/*                                                                          */
+/* data structures exported by the corresponding source file                */
+/*                                                                          */
 /****************************************************************************/
 
-struct BVPDescriptor
+typedef void *BVP;                 /* structure handled by domain module    */
+typedef void *BNDS;                /* structure handled by domain module    */
+typedef void *BNDP;                /* structure handled by domain module    */
+
+struct BVP_Descriptor
 {
   /* general part */
-  char name[NAMELEN];
+  char name[NAMELEN];                /* name of the BVP                     */
 
   /* domain part */
-  COORD midpoint[DIM];
+  COORD midpoint[DIM];               /* sphere in which the domain lies     */
   COORD radius;
-  INT nCorners;
-  INT convex;
-  INT nPatches;
-  INT nSubDomains;
-  INT id;
+  INT convex;                        /* 1 if domain is convex, 0 if not     */
+  INT nSubDomains;                   /* nb. of subdomains,
+                                                                                exterior not counted                */
 
   /* problem part */
-  ConfigProcPtr ConfigProc;
-  INT numOfCoeffFct;
-  INT numOfUserFct;
+  ConfigProcPtr ConfigProc;          /* configuration function              */
+  INT numOfCoeffFct;                 /* nb. of coefficient functions        */
+  INT numOfUserFct;                  /* nb. of user functions               */
 };
+typedef struct BVP_Descriptor BVP_DESC;
 
-struct patch_corner
+struct mesh
 {
-  INT id;
-  COORD lcoord[DIM_OF_BND];
+  INT nBndP;                         /* nb. of boundary points              */
+  BNDP **theBndPs;                                       /* list of boundary points	            */
+  INT nInnP;                         /* nb. of inner nodes                  */
+  COORD **Position;                  /* positions of inner nodes            */
+  INT nSubDomains;                   /* nb. of subdomains                   */
+  INT *nSides;                       /* nb. of boundary sides per subdomain */
+  INT **Side_corners;                /* nb. of side corners                 */
+  INT ***Side_corner_ids;                /* corner ids                          */
+  INT *nElements;                    /* nb. of element corners              */
+  INT **Element_corners;             /* nb. of element corners              */
+  INT ***Element_corner_ids;         /* nb. of side corners                 */
+  INT ***nbElements;                 /* nb. of side corners                 */
 };
-
-struct PatchDescriptor
-{
-  INT left, right;
-  INT type;
-  INT n;
-  INT id;
-  INT resolution;
-  struct patch_corner pc[CORNERS_OF_BND_SEG];
-};
-
-typedef struct BVPDescriptor BVP_DESC;
-typedef struct PatchDescriptor PATCH_DESC;
-
-/* dummies for ug */
-typedef char BVP;
-typedef char PATCH;
+typedef struct mesh MESH;
 
 /****************************************************************************/
-/*																			*/
-/* definition of exported global variables									*/
-/*																			*/
+/*                                                                          */
+/* definition of exported global variables                                  */
+/*                                                                          */
 /****************************************************************************/
 
 /****************************************************************************/
-/*																			*/
-/* function declarations: IF-functions defined in the module				*/
-/*																			*/
+/*                                                                          */
+/* function declarations                                                    */
+/*                                                                          */
 /****************************************************************************/
 
-/* functionallity for BVPs (BoundaryValueProblems) */
-BVP               *GetBVP                                                       (char *name);
-INT        BVP_SetBVPDesc                               (BVP *theBVP, BVP_DESC *theBVPDesc);
-PATCH     *BVP_GetFirstPatch                            (BVP *theBVP);
-PATCH     *BVP_GetNextPatch                             (BVP *theBVP, PATCH *thePatch);
-INT                BVP_SetCoeffFct                                      (BVP *theBVP, INT n, CoeffProcPtr *CoeffFct);
-INT                BVP_SetUserFct                                       (BVP *theBVP, INT n, UserProcPtr *UserFct);
+#ifdef ModelP
 
-/* functionallity for PATCHes */
-INT                Patch_GetPatchDesc                           (PATCH *thePatch, PATCH_DESC *thePatchDesc);
-INT        Patch_global2local                   (PATCH *thePatch, COORD *global, COORD *local);
-INT        Patch_local2global                   (PATCH *thePatch, COORD *local, COORD *global);
-INT                Patch_local2bndcond                          (PATCH *thePatch, COORD *local, DOUBLE *value, INT *type);
+void DomInitParallel     (INT TypeBndP, INT TypeBndS);
+void DomHandlerInit      (INT handlerSet);
+
+void BElementXferBndS    (BNDS **bnds, int n, int proc, int prio);
+void BElementGatherBndS  (BNDS **bnds, int n, int cnt, char *data);
+void BElementScatterBndS (BNDS **bnds, int n, int cnt, char *data);
+
+void BVertexXferBndP     (BNDP *bndp, int proc, int prio);
+void BVertexGatherBndP   (BNDP *bndp, int cnt, char *data);
+void BVertexScatterBndP  (BNDP **bndp, int cnt, char *data);
+
+#endif
+
+/* functions for BVP */
+BVP        *BVP_GetFirst          (void);
+BVP        *BVP_GetNext           (BVP *theBVP);
+INT         BVP_Save              (BVP *theBVP, char *name, INT argc, char **argv);
+BVP        *BVP_Load              (char *name, INT argc, char **argv);
+BVP        *BVP_GetByName         (char *name);
+BVP        *BVP_Init              (char *name, HEAP *Heap, MESH *Mesh);
+INT         BVP_SetBVPDesc        (BVP *theBVP, BVP_DESC *theBVPDesc);
+INT         BVP_SetCoeffFct       (BVP *theBVP, INT n, CoeffProcPtr *CoeffFct);
+INT         BVP_SetUserFct        (BVP *theBVP, INT n, UserProcPtr *UserFct);
+
+/* functions called by script commands */
+BNDP*           BVP_InsertBndP            (HEAP *Heap, BVP *theBVP, INT argc, char **argv);
+INT         BNDP_SaveInsertedBndP (BNDP *theBndP, char *data, INT max_data_size);
+MESH       *BVP_GenerateMesh      (HEAP *Heap, BVP *theBVP, INT argc, char **argv);
+
+/* functions for BNDP */
+INT         BNDP_Global           (BNDP *theBndP, COORD *global);
+INT         BNDP_BndCond          (BNDP *theBndP, INT *n, INT i, COORD *in, DOUBLE *value, INT *type);
+INT         BNDP_BndPDesc         (BNDP *theBndP, INT *move);
+BNDS*       BNDP_CreateBndS       (HEAP *Heap, BNDP **theBndP, INT n);
+BNDP*       BNDP_CreateBndP       (HEAP *Heap, BNDP *theBndP0, BNDP *theBndP1, COORD lcoord);
+INT         BNDP_Dispose          (HEAP *Heap, BNDP *theBndP);
+INT         BNDP_SaveBndP         (BNDP *theBndP, char *data, INT *data_size);
+BNDP       *BNDP_LoadBndP         (HEAP *Heap, char *data, INT *data_size);
+
+/* functions for BNDS */
+INT         BNDS_Global           (BNDS *theBndS, COORD *local, COORD *global);
+INT         BNDS_BndCond          (BNDS *theBndS, COORD *local, COORD *in, DOUBLE *value, INT *type);
+INT         BNDS_BndSDesc         (BNDS *theBndS, INT *left, INT *right);
+BNDP*       BNDS_CreateBndP       (HEAP *Heap, BNDS *theBndS, COORD *local);
+INT         BNDS_Dispose          (HEAP *Heap, BNDS *theBndS);
+INT         BNDS_SaveBndS         (HEAP *Heap, BNDS *theBndS, char *data, INT *data_size);
+BNDS       *BNDS_LoadBndS         (HEAP *Heap, char *data, INT *data_size);
 
 /* miscellanious */
-INT                InitDom                                                      (void);
+INT         InitDom               (void);
 
-/****************************************************************************/
-/*																			*/
-/* function declarations: derived function									*/
-/*																			*/
-/****************************************************************************/
-
-PATCH      *Patch_GetPatchByID                          (BVP *theBVP, INT id);
-INT             Patch_GetPatchID                                (PATCH *thePatch);
 #endif
