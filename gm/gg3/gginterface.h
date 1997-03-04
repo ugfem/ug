415f7c377c51473bf7c19d741641dfad547a899c
klaus <klaus>
1997-03-04 19:39:03 +0000

    bug removed
    [[Imported from SVN: r2080]]

--- gm/gg3/gginterface.h
+++ gm/gg3/gginterface.h
@@ -35,9 +35,18 @@
 /****************************************************************************/
 
 int AddInnerNode (double x, double y, double z);
+int AddInnerNode2ug (double x, double y, double z);
 int AddTetrahedron (int node0, int node1, int node2, int node3);
 int AddSurfaceNode (int nodeid, double x, double y, double z);
 int AddSurfaceTriangle (int node0, int node1, int node2);
+int AddSurfaceTriangle2ug (int node0, int node1, int node2);
 int InitNetgen (char *rulefilename);
 int StartNetgen (double h,int smooth,int display);
+
+int AddGeomPoint (int nodeid, double x, double y, double z);
+int AddGeomElement (int node0, int node1, int node2);
+int AddLinePoint (int id, double x, double y, double z);
+int AddLineSegment (int i1,int i2);
+int InitSurfaceNetgen (char *rulefilename);
+int StartSurfaceNetgen (double h,int smooth,int display);
 #endif
