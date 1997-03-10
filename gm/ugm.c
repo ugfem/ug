a6bd72190930f32c53ab27505d73eadddbff7b08
klaus <klaus>
1997-03-10 17:42:15 +0000

    check erweitert
    [[Imported from SVN: r2122]]

--- gm/ugm.c
+++ gm/ugm.c
@@ -5355,6 +5355,11 @@ static INT CheckElement (ELEMENT *theElement, INT *SideError, INT *EdgeError,
 
       if (OBJT(theElement) == BEOBJ)
       {
+        if (INNER_BOUNDARY(theElement,i)) {
+          *SideError |= (1<<(i+2*MAX_SIDES_OF_ELEM));
+          UserWriteF(PFMT "no nb Element for inner boundary, el =  " EID_FMTX "\n",
+                     me,EID_PRTX(theElement));
+        }
         if (SIDE_ON_BND(theElement,i))
         {
           for (j=0; j<CORNERS_OF_SIDE(theElement,i); j++)
