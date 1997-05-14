f959e94cf0cfd768f1b10e8b0944d90061b0ae30
wieners <wieners>
1997-05-14 06:31:38 +0000

    *** empty log message ***
    [[Imported from SVN: r2521]]

--- ui/cmdint.c
+++ ui/cmdint.c
@@ -2583,8 +2583,9 @@ void CommandLoop (int argc, char **argv)
       {
         sprintf(inpLine,"execute %s\n",argv[i]);
         InterpretCommand(inpLine);         /* execute command line argument */
-        if (strcmp(argv[i+1],"-noquit") == 0)
-          CommandLoop(0,NULL);
+        if (i + 1 < argc)
+          if (strcmp(argv[i+1],"-noquit") == 0)
+            CommandLoop(0,NULL);
         InterpretCommand("quit\n");        /* end program */
         i++;
         continue;
