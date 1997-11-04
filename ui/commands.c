9e229e29cec96340dc9d622d8e71603532f719e6
wieners <wieners>
1997-11-04 14:13:17 +0000

    timings erweitert
    [[Imported from SVN: r4159]]

--- ui/commands.c
+++ ui/commands.c
@@ -11595,10 +11595,14 @@ static INT TimingCommand (INT argc, char **argv)
         #else
     fac = 1.0 / ((double)CLOCKS_PER_SEC);
         #endif
-    for (i=0; i<debug_time_count; i++)
-      UserWriteF("%2d: File: %20s, Line: %5d elapsed time %10.4f\n",
+    for (i=0; i<debug_time_count; i++) {
+      UserWriteF("%2d: File:%15s, Line:%5d elapsed time%10.4f",
                  i,debug_time_file[i],debug_time_line[i],
                  (debug_time[i]-debug_time[0])*fac);
+      if (i > 0) UserWriteF(" diff%8.4f",
+                            (debug_time[i]-debug_time[i-1])*fac);
+      UserWriteF("\n");
+    }
   }
   return (OKCODE);
 }
