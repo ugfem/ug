3aac9188670a39c5d2fcb92740326a7204be52e9
birken <birken>
1997-12-16 16:23:36 +0000

    coviseif is now a C++ file. new version (still preliminary) by SL, KB and Uwe Woessner from RUS.
    Makefile.covise has hardcoded library path, should be removed in
    next version.
    
    contains debug output. coviseif is able to connect via sockets
    (aka Covise Commlib) to a covise frontend running on some workstation.
    
    [[Imported from SVN: r4529]]

--- graphics/covise/coviseif.cpp
+++ graphics/covise/coviseif.cpp
@@ -2,7 +2,7 @@
 // vi: set et ts=4 sw=2 sts=2:
 /****************************************************************************/
 /*																			*/
-/* File:	  coviseif.c                                                                                                    */
+/* File:	  coviseif.cpp                                                                                                  */
 /*																			*/
 /* Purpose:   interface ug <-> covise                                                   */
 /*																			*/
@@ -30,13 +30,21 @@
 #include <string.h>
 
 
-/* ug includes */
+extern "C" {
+
+  /* ug includes */
+#define ListElement UG_ListElement
 #include "gm.h"
 #include "evm.h"
 #include "general.h"
+#undef ListElement
+
+} /* extern "C" */
+
 
 /* covise includes */
-/*#include "covise.h"*/
+#include "covise_connect.h"
+#include "covise_msg.h"
 
 
 
@@ -54,6 +62,18 @@
 #define MAX_SOLUTIONS  16
 #define MAX_COMPONENTS 3
 
+#define MAX_ITEMS_SENT 4096
+
+
+/* communication constants */
+#define DFLT_PORT      31700
+
+
+enum CommonConsts {
+  MT_NONE,
+  MT_UGHEADER
+};
+
 
 /****************************************************************************/
 /*																			*/
@@ -61,6 +81,13 @@
 /*		  in the corresponding include file!)								*/
 /*																			*/
 /****************************************************************************/
+
+
+/* common data types (for communication UG<->Covise) */
+typedef int INT32;
+typedef float FLOAT32;
+
+
 
 typedef struct
 {
@@ -93,6 +120,8 @@ typedef struct
 } COVISE_HEADER;
 
 
+
+
 /****************************************************************************/
 /*																			*/
 /* definition of exported global variables									*/
@@ -108,17 +137,10 @@ typedef struct
 /****************************************************************************/
 
 /* ug: */
-/* not used currently
-   static EVALUES *ElemEval;
-   static EVECTOR *ElemVec;
-   static PreprocesingProcPtr EvalPreProc;
-   static ElementEvalProcPtr EvalPlotProc;
-   static ElementVectorProcPtr EvecPlotProc;
-   static int FirstEvec;
- */
 
 
 /* covise: */
+static ClientConnection* covise_connection=NULL;
 
 
 /* RCS string */
@@ -134,8 +156,8 @@ static char RCS_ID("$Header$",UG_RCS_STRING);
 
 /****************************************************************************/
 /*																			*/
-/* functions
-   /*																			*/
+/* functions																*/
+/*																			*/
 /****************************************************************************/
 
 
@@ -211,7 +233,10 @@ static INT GetSolutionDescs (MULTIGRID *theMG, COVISE_HEADER *covise)
   strcpy(covise->solutions[1].name, "Pressure");
   covise->solutions[1].n_components = 1;
   covise->solutions[1].comps[0]      = 1;
+
+  return(0);
 }
+
 
 
 static INT FillCoviseHeader (MULTIGRID *theMG, COVISE_HEADER *covise)
@@ -255,15 +280,58 @@ static INT ResetVertexFlags (MULTIGRID *theMG, INT min_level, INT max_level)
 }
 
 
-static INT SendSurfaceGrid (MULTIGRID *theMG, INT min_level, INT max_level)
+/* TODO renumber doesn't work in parallel */
+static INT RenumberVertices (MULTIGRID *theMG, INT min_level, INT max_level)
 {
-  INT l;
+  INT l, i;
 
-  /* send surface vertices, part1: set flags */
   ResetVertexFlags(theMG, min_level, max_level);
 
-  /* send surface vertices, part2: send data */
+  /* reset used flags in vertices */
+  i = 0;
   for (l=min_level; l<=max_level; l++)
+  {
+    NODE *theNode;
+    GRID *theGrid = GRID_ON_LEVEL(theMG,l);
+
+    /* reset USED flags in all vertices */
+    for (theNode=FIRSTNODE(theGrid); theNode!=NULL; theNode=SUCCN(theNode))
+    {
+      if (USED(MYVERTEX(theNode))) continue;
+      SETUSED(MYVERTEX(theNode),1);
+
+      ID(MYVERTEX(theNode)) = i;
+      i++;
+    }
+  }
+
+  return(0);
+}
+
+
+static INT SendSurfaceGrid (MULTIGRID *theMG, COVISE_HEADER *covise)
+{
+  INT l, remaining, sent;
+  TokenBuffer tb;
+  Message* msg = new Message(tb);
+  msg->type = (covise_msg_type)0;
+
+  /* renumber vertex IDs */
+  /* TODO doesn't work in ModelP */
+  RenumberVertices(theMG, covise->min_level, covise->max_level);
+
+  /* send surface vertices, part1: set flags */
+  ResetVertexFlags(theMG, covise->min_level, covise->max_level);
+
+  /* send surface vertices, part2: send data */
+  sent = 0;
+
+  /* start first buffer */
+  tb.reset();
+  remaining = MIN(covise->n_vertices,MAX_ITEMS_SENT);
+  tb << remaining;
+
+  for (l=covise->min_level; l<=covise->max_level; l++)
   {
     NODE *theNode;
     GRID *theGrid = GRID_ON_LEVEL(theMG,l);
@@ -277,19 +345,39 @@ static INT SendSurfaceGrid (MULTIGRID *theMG, INT min_level, INT max_level)
       SETUSED(MYVERTEX(theNode),1);
 
       /* extract data from vertex */
-      vid = ID(MYVERTEX(theNode));
-      /* TODO write vid to buffer */
       /* TODO use VXGID in ModelP */
-
+      vid = ID(MYVERTEX(theNode));
       pos = CVECT(MYVERTEX(theNode));
-      /* TODO write pos[DIM] to buffer */
+
+      tb << (INT32)vid;
+      tb << (FLOAT32)pos[0];
+      tb << (FLOAT32)pos[1];
+      tb << (FLOAT32)pos[2];
+      remaining--;
+      sent++;
+
+      if (remaining==0)
+      {
+        /* send this buffer */
+        covise_connection->send_msg(msg);
+
+        /* start next buffer */
+        tb.reset();
+        remaining = MIN(covise->n_vertices - sent, MAX_ITEMS_SENT);
+        tb << remaining;
+      }
     }
   }
 
 
+  /* next buffer */
+  tb.reset();
+  remaining = MIN(covise->n_elems,MAX_ITEMS_SENT);
+  tb << remaining;
+  sent = 0;
 
   /* send surface elems and connectivity */
-  for (l=min_level; l<=max_level; l++)
+  for (l=covise->min_level; l<=covise->max_level; l++)
   {
     ELEMENT *theElement;
     GRID *theGrid = GRID_ON_LEVEL(theMG,l);
@@ -297,11 +385,11 @@ static INT SendSurfaceGrid (MULTIGRID *theMG, INT min_level, INT max_level)
     for (theElement=FIRSTELEMENT(theGrid);
          theElement!=NULL; theElement=SUCCE(theElement))
     {
-      if ((EstimateHere(theElement)) || (l==max_level))
+      if ((EstimateHere(theElement)) || (l==covise->max_level))
       {
         int i, coe = CORNERS_OF_ELEM(theElement);
 
-        /* TODO write coe to buffer (elem) */
+        tb << (INT32)coe;
 
         for (i=0; i<coe; i++)
         {
@@ -309,55 +397,245 @@ static INT SendSurfaceGrid (MULTIGRID *theMG, INT min_level, INT max_level)
           INT vid;
 
           vid = ID(MYVERTEX(theNode));
-          /* TODO write vid to buffer (conn) */
           /* TODO use VXGID in ModelP */
+          tb << (INT32)vid;
+        }
+
+        remaining--;
+        sent++;
+
+        if (remaining==0)
+        {
+          /* send this buffer */
+          covise_connection->send_msg(msg);
+
+          /* start next buffer */
+          tb.reset();
+          remaining = MIN(covise->n_elems - sent, MAX_ITEMS_SENT);
+          tb << remaining;
         }
       }
     }
   }
+
+  /* cleanup */
+  delete msg;
+
+  return(0);
 }
 
 
 
 static INT SendSolution (MULTIGRID *theMG, COVISE_HEADER *covise, INT idx_sol)
 {
-  INT l;
+  INT l, remaining, sent, n_comp_sol;
+  TokenBuffer tb;
+  Message* msg = new Message(tb);
+  msg->type = (covise_msg_type)0;
+
+  n_comp_sol = covise->solutions[idx_sol].n_components;
 
   /* reset vertex flags */
   ResetVertexFlags(theMG, covise->min_level, covise->max_level);
 
 
-  /* extract data, loop from max_level to min_level! */
-  /* TODO: special handling in ModelP */
-  for (l=covise->max_level; l>=covise->min_level; l--)
-  {
-    NODE *theNode;
-    GRID *theGrid = GRID_ON_LEVEL(theMG,l);
+  /* start first buffer */
+  tb.reset();
+  remaining = MIN(covise->n_vertices,MAX_ITEMS_SENT);
+  tb << remaining;
+  tb << idx_sol;
+  sent = 0;
 
-    for (theNode=FIRSTNODE(theGrid); theNode!=NULL; theNode=SUCCN(theNode))
+  for (l=covise->min_level; l<=covise->max_level; l++)
+
+    /* extract data, loop from max_level to min_level! */
+    /* TODO: special handling in ModelP */
+    for (l=covise->max_level; l>=covise->min_level; l--)
     {
-      VECTOR *theVector;
-      INT i;
+      NODE *theNode;
+      GRID *theGrid = GRID_ON_LEVEL(theMG,l);
 
-      if (USED(MYVERTEX(theNode))) continue;
-      SETUSED(MYVERTEX(theNode),1);
-
-      theVector = NVECTOR(theNode);
-
-      /* extract data from vector */
-      for(i=0; i<covise->solutions[idx_sol].n_components; i++)
+      for (theNode=FIRSTNODE(theGrid); theNode!=NULL; theNode=SUCCN(theNode))
       {
-        INT comp = covise->solutions[idx_sol].comps[i];
-        DOUBLE val = VVALUE(theVector,comp);
-        /* TODO write val to buffer */
+        VECTOR *theVector;
+        INT i;
+
+        if (USED(MYVERTEX(theNode))) continue;
+        SETUSED(MYVERTEX(theNode),1);
+
+        theVector = NVECTOR(theNode);
+
+        /* extract data from vector */
+        for(i=0; i<n_comp_sol; i++)
+        {
+          INT comp = covise->solutions[idx_sol].comps[i];
+          tb << (FLOAT32) VVALUE(theVector,comp);
+        }
+
+        remaining--;
+        sent++;
+
+        if (remaining==0)
+        {
+          /* send this buffer */
+          covise_connection->send_msg(msg);
+
+          /* start next buffer */
+          tb.reset();
+          remaining = MIN(covise->n_vertices - sent, MAX_ITEMS_SENT);
+          tb << remaining;
+          tb << idx_sol;
+        }
       }
     }
-  }
+
+  delete msg;
+  return(0);
 }
 
 
 
 /****************************************************************************/
+
+
+/****************************************************************************/
+
+
+
+extern "C" INT ConnectCovise (MULTIGRID *theMG, char *hostname)
+{
+  COVISE_HEADER covise;
+  Host *remotehost=new Host(hostname);
+
+  printf("CoviseIF: trying to connect to %s\n", hostname);
+
+  if (covise_connection==NULL)
+  {
+    covise_connection =
+      new ClientConnection(remotehost, DFLT_PORT, 0, (sender_type)0, 0);
+    printf("CoviseIF: new ClientConnection =%08x\n");
+
+    if (! covise_connection->is_connected())
+    {
+      delete covise_connection;
+      covise_connection=NULL;
+    }
+  }
+
+  if (covise_connection!=NULL && covise_connection->is_connected())
+  {
+    printf("CoviseIF: ok, connected to %s\n", hostname);
+    Message* msg = new Message();
+    msg->type = (covise_msg_type)0;
+    int i;
+
+    if (covise_connection->check_for_input())
+    {
+      printf("CoviseIF: input from %s\n", hostname);
+      covise_connection->recv_msg(msg);
+      printf("CoviseIF: received msg from %s, type %d\n", hostname, msg->type);
+
+      switch (msg->type)
+      {
+      case (covise_msg_type)0 :
+      {
+        TokenBuffer tb(msg);
+        int headerRequest,gridRequest,numSolutions,solutions[MAX_SOLUTIONS];
+        tb >> headerRequest;
+        tb >> gridRequest;
+        tb >> numSolutions;
+        for(i=0; i<numSolutions; i++)
+          tb >> solutions[i];
+
+        if (headerRequest)
+        {
+          printf("CoviseIF: headerRequest\n");
+          TokenBuffer rtb;
+          FillCoviseHeader(theMG, &covise);
+          rtb << MT_UGHEADER;
+          rtb << covise.min_level;
+          rtb << covise.max_level;
+          rtb << covise.n_vertices;
+          rtb << covise.n_elems;
+          rtb << covise.n_conns;
+          rtb << covise.num_solutions;
+          for(i=0; i<numSolutions; i++)
+          {
+            rtb << covise.solutions[i].n_components;
+            rtb << covise.solutions[i].name;
+          }
+
+          Message rmsg(rtb);
+          rmsg.type = (covise_msg_type)0;
+          covise_connection->send_msg(&rmsg);
+        }
+
+        if (gridRequest)
+        {
+          printf("CoviseIF: gridRequest\n");
+          SendSurfaceGrid(theMG, &covise);
+        }
+
+        if (numSolutions>0)
+        {
+          printf("CoviseIF: solRequest\n");
+          for(i=0; i<numSolutions; i++)
+          {
+            SendSolution(theMG,&covise,solutions[i]);
+          }
+        }
+      }
+      break;
+
+      case SOCKET_CLOSED :
+      case CLOSE_SOCKET :
+      case EMPTY :
+      {
+        printf("CoviseIF: SOCKET_CLOSED\n");
+        delete covise_connection;
+        covise_connection = NULL;
+      }
+      break;
+
+      case QUIT :
+      {
+        printf("CoviseIF: QUIT\n");
+        delete covise_connection;
+        covise_connection = NULL;
+        /* TODO exit UG itself */
+      }
+      break;
+      }
+    }
+
+    delete msg;
+  }
+
+
+  /* cleanup */
+  delete remotehost;
+
+  return(0);       /* no error */
+}
+
+
+
+extern "C" INT InitCoviseIF (void)
+{
+  return(0);       /* no error */
+}
+
+
+extern "C" INT ExitCoviseIF (void)
+{
+  if (covise_connection!=NULL)
+  {
+    delete covise_connection;
+    covise_connection=NULL;
+  }
+
+  return(0);       /* no error */
+}
 
 
 /****************************************************************************/
