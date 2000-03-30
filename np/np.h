39b5572ad466919dcd07c1ae4077780672991d2b
klaus <klaus>
2000-03-30 08:17:08 +0000

    diagonal consistency for ssor, sor in parallel
    [[Imported from SVN: r6700]]

--- np/np.h
+++ np/np.h
@@ -397,12 +397,12 @@ INT l_ugs                       (GRID *g, const VECDATA_DESC *v, const MATDATA_D
 INT l_lgsB                      (GRID *g, const VECDATA_DESC *v, const MATDATA_DESC *M, const VECDATA_DESC *d);
 INT gs_solveBS          (const BLOCKVECTOR *bv, const BV_DESC *bvd, const BV_DESC_FORMAT *bvdf, DOUBLE eps, INT max_it, INT K_comp, INT u_comp, INT f_comp, INT aux_comp, INT verbose, INT eps_relative );
 
-INT l_lsor                      (GRID *g, const VECDATA_DESC *v, const MATDATA_DESC *M, const VECDATA_DESC *d, const DOUBLE *damp);
+INT l_lsor                      (GRID *g, const VECDATA_DESC *v, const MATDATA_DESC *M, const VECDATA_DESC *d, const DOUBLE *damp, VECDATA_DESC *diag);
 INT l_usor (GRID *g, const VECDATA_DESC *v, const MATDATA_DESC *M,
-            const VECDATA_DESC *d, const DOUBLE *omega);
-INT l_lsor_ld       (GRID *g, const VECDATA_DESC *v, const MATDATA_DESC *M, const VECDATA_DESC *d, const VECDATA_DESC *damp);
+            const VECDATA_DESC *d, const DOUBLE *omega, VECDATA_DESC *diag);
+INT l_lsor_ld       (GRID *g, const VECDATA_DESC *v, const MATDATA_DESC *M, const VECDATA_DESC *d, const VECDATA_DESC *damp, VECDATA_DESC *diag);
 INT l_usor_ld (GRID *g, const VECDATA_DESC *v, const MATDATA_DESC *M,
-               const VECDATA_DESC *d, VECDATA_DESC *omega);
+               const VECDATA_DESC *d, VECDATA_DESC *omega, VECDATA_DESC *diag);
 
 
 /* iterative methods for Simple BLOCKVECTOR */
