8fdbd8598136a2c93a0d7bff3abecd4e0f3c62ff
klaus <klaus>
2000-11-21 11:44:37 +0000

    reinit-numproc to reinit an application
    [[Imported from SVN: r6968]]

---
+++ np/procs/reinit.c
@@ -0,0 +1,113 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+/****************************************************************************/
+/*                                                                          */
+/* File:      tstep.c                                                           */
+/*                                                                          */
+/* Purpose:   time-step schemes                                                                 */
+/*                                                                          */
+/* Author:    Klaus Johannsen                                                                                           */
+/*            Institut fuer Computeranwendungen III                         */
+/*            Universitaet Stuttgart                                        */
+/*            Pfaffenwaldring 27                                            */
+/*            70569 Stuttgart                                               */
+/*            email: ug@ica3.uni-stuttgart.de                               */
+/*                                                                          */
+/* History:   Jan  31, 1998  begin                                          */
+/*                                                                          */
+/* Remarks:                                                                 */
+/*                                                                          */
+/****************************************************************************/
+
+/****************************************************************************/
+/*                                                                          */
+/* include files                                                            */
+/*            system include files                                          */
+/*            application include files                                     */
+/*                                                                          */
+/****************************************************************************/
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#include "ugdevices.h"
+#include "general.h"
+#include "debug.h"
+#include "ugstruct.h"
+#include "misc.h"
+#include "gm.h"
+#include "evm.h"
+#include "scan.h"
+#include "numproc.h"
+#include "pcr.h"
+#include "np.h"
+
+#include "nls.h"
+#include "ls.h"
+#include "nls.h"
+#include "assemble.h"
+#include "transfer.h"
+
+#include "reinit.h"
+
+/****************************************************************************/
+/*                                                                          */
+/* defines in the following order                                           */
+/*                                                                          */
+/*        compile time constants defining static data size (i.e. arrays)    */
+/*        other constants                                                   */
+/*        macros                                                            */
+/*                                                                          */
+/****************************************************************************/
+
+REP_ERR_FILE;
+
+/* RCS string */
+static char RCS_ID("$Header$",UG_RCS_STRING);
+
+/****************************************************************************/
+/*                                                                          */
+/* generic routines                                                         */
+/*                                                                          */
+/****************************************************************************/
+
+INT REINIT_Display (NP_BASE *base)
+{
+  NP_REINIT *reinit;
+  INT i;
+
+  reinit=(NP_REINIT*)base;
+  UserWrite("\nreinit configuration:\n");
+  for (i=0; i<reinit->n; i++)
+    UserWriteF(DISPLAY_NP_FORMAT_SF,reinit->name[i],(float)reinit->parameter[i]);
+
+  return (0);
+}
+
+/****************************************************************************/
+/*
+   InitReinit  - Init this file
+
+   SYNOPSIS:
+   INT InitReinit ();
+
+   PARAMETERS:
+   .  void -
+
+   DESCRIPTION:
+   This function inits this file.
+
+   RETURN VALUE:
+   INT
+   .n    0 if ok
+   .n    __LINE__ if error occured.
+ */
+/****************************************************************************/
+
+INT InitReinit ()
+{
+  return (0);
+}
---
+++ np/procs/reinit.h
@@ -0,0 +1,97 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+/****************************************************************************/
+/*                                                                          */
+/* File:      reinit.h                                                          */
+/*                                                                          */
+/* Purpose:   numproc interface to reinit problem classes                   */
+/*                                                                          */
+/* Author:    Klaus Johannsen                                               */
+/*            IWR/TS                                                        */
+/*            Universitaet Heidelberg                                       */
+/*            Im Neuenheiner Feld 368                                       */
+/*            69120 Heidelberg                                              */
+/*            email: ug@ica3.uni-stuttgart.de                               */
+/*                                                                          */
+/* History:   August 16, 2000                                               */
+/*                                                                          */
+/* Remarks:                                                                 */
+/*                                                                          */
+/****************************************************************************/
+
+
+/* RCS_ID
+   $Header$
+ */
+
+/****************************************************************************/
+/*																			*/
+/* auto include mechanism and other include files							*/
+/*																			*/
+/****************************************************************************/
+
+#ifndef __REINIT__
+#define __REINIT__
+
+#include "np.h"
+
+/****************************************************************************/
+/*																			*/
+/* defines in the following order											*/
+/*																			*/
+/*		  compile time constants defining static data size (i.e. arrays)	*/
+/*		  other constants													*/
+/*		  macros															*/
+/*																			*/
+/****************************************************************************/
+
+#define REINIT_CLASS_NAME "reinit"
+
+/****************************************************************************/
+/*																			*/
+/* definition of exported data structures									*/
+/*																			*/
+/****************************************************************************/
+
+#define PARAMETER_MAX               10
+
+struct reinit_result {
+
+  INT parameter_not_found;              /* 1 if parameter not found         */
+  INT parameter_out_of_range;           /* 1 if parameter out of range      */
+  INT parameter_nochange;                   /* 1 if no change performed         */
+};
+typedef struct reinit_result REINIT_RESULT;
+
+struct np_reinit {
+
+  NP_BASE base;                              /* derived from nonlinear assemble */
+
+  /* things to be initialized for being executable */
+  INT n;                                     /* number of parameters            */
+  char name[NAMESIZE][PARAMETER_MAX];        /* name of parameters              */
+  DOUBLE parameter[PARAMETER_MAX];           /* parameters                      */
+
+  /* functions */
+  INT (*GetProblemParameter)
+    (struct np_reinit *,                 /* pointer to (derived) object     */
+    char *,                              /* name of parameter               */
+    DOUBLE *);                                       /* parameter                               */
+  INT (*ReinitProblem)
+    (struct np_reinit *,                     /* pointer to (derived) object     */
+    char *,                              /* name of parameters              */
+    DOUBLE,                                      /* parameters                          */
+    REINIT_RESULT *);                    /* result                          */
+};
+typedef struct np_reinit NP_REINIT;
+
+/****************************************************************************/
+/*																			*/
+/* definition of exported functions											*/
+/*																			*/
+/****************************************************************************/
+
+INT REINIT_Display (NP_BASE *base);
+INT InitReinit (void);
+
+#endif
