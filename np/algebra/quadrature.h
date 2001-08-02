c8d4b026b5ecf9648767340ae79ae573851ba7cf
reichenb <reichenb>
2001-08-02 14:12:00 +0000

    Declare function GetSymmetricQuadratureRule(), which returns only quadrature rules with axial symmetric integration points
    [[Imported from SVN: r7037]]

--- np/algebra/quadrature.h
+++ np/algebra/quadrature.h
@@ -94,6 +94,7 @@ typedef struct {
 
 QUADRATURE *GetQuadrature(INT dim, INT n, INT order);
 QUADRATURE *GetQuadratureRule(INT dim, INT n, INT order);
+QUADRATURE *GetSymmetricQuadratureRule(INT dim, INT n, INT order);
 INT GaussPoints(INT dim, INT n, INT order, DOUBLE_VECTOR *x, GAUSS_POINT *gp);
 
 #endif
