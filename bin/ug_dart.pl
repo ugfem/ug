8c73f835c17345b0d8e0bc40644c830c2e3c7d60
ugtest <ugtest>
2004-02-03 09:32:05 +0000

    testtypes: NIGHTLY CONTINUOUS EXPERIMENTAL
    [[Imported from SVN: r7511]]

--- bin/ug_dart.pl
+++ bin/ug_dart.pl
@@ -16,6 +16,13 @@ if($ARGV[0] eq "-help" || $ARGV[0] eq "-h")
 	print "         build/test/submit cycle\n";
 	die "\n";
 }
+
+my $test_type = "Nightly";
+if($ARGV[0] eq "Experimental" || $ARGV[0] eq "Nightly" || $ARGV[0] eq "Continuous")
+{
+	$test_type = splice(@ARGV,0,1);
+}
+
 my $mode = "Client";
 if($ARGV[0] eq "Client" || $ARGV[0] eq "Server")
 {
@@ -28,15 +35,18 @@ foreach $param (@ARGV)
 }
 if($mode eq "Client")
 {
-#	system("ug_dart_co $mode");
+       # checkout only for the first build/test/submit cycle			
+        #system("ug_dart_co $mode");
 	system("$command");
 	system("ug_dart_tests.pl");
-	system("ug_dart_client_test");
+	system("ug_dart_client_test $test_type");
 }
 elsif($mode eq "Server")
 {
-#   system("ug_dart_co $mode");  
-	system("ug_dart_tree");
+       # checkout only for the first build/test/submit cycle 
+        #system("ug_dart_co $mode");  
+       # generate the build tree only for the first build/test/submit cycle
+        #system("ug_dart_tree");
 	system("$command");
-	system("ug_dart_server_start");
+	system("ug_dart_server_start $test_type");
 }	
