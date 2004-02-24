2c35f763b3ba7f9d067f0dfe607c90d0342a799a
ugtest <ugtest>
2004-02-24 09:55:47 +0000

    - ug_dart.pl                    added two more command line                                         parameters: test type(Experimental,                                         Continuous, Nightly), build directory
    [[Imported from SVN: r7523]]

--- bin/ug_dart.pl
+++ bin/ug_dart.pl
@@ -5,9 +5,14 @@
 # purpose: runs all necessary scripts for either the server (mode=Server)
 #          or the client (mode=Client)
 
-if($ARGV[0] eq "-help" || $ARGV[0] eq "-h")
+# print help message 
+if($ARGV[0] eq "-help" || $ARGV[0] eq "--help" || $ARGV[0] eq "-h")
 {
-	print "usage:   ug_dart.pl modeparameter parameter1 ... parametern\n";
+	print "usage:	ug_dart.pl -bg build_dir_suffix test_type modeparameter\n";
+        print "                    architecture parameter1 ... parametern\n";
+	print "	 -bg build_dir_suffix: specifying another build directory than .../UG by entering the\n";
+	print "        	              sub directory, e.g. cd/appl\n"; 
+	print "  	 test_type: May be Nightly, Continuous or Experimental\n";
 	print "         modeparameter: calling this script on the server: modeparameter = Server\n";
 	print "                        calling this script on the client: modeparameter = Client\n";
 	print "         parameter1 ... parametern: same parameters as ugconf\n";
@@ -16,30 +21,60 @@ if($ARGV[0] eq "-help" || $ARGV[0] eq "-h")
 	print "         build/test/submit cycle\n";
 	die "\n";
 }
+# set up a variable to count the given parameters
+my $param_count = 0;
 
-my $test_type = "Nightly";
-if($ARGV[0] eq "Experimental" || $ARGV[0] eq "Nightly" || $ARGV[0] eq "Continuous")
+# specify the build directory
+my $build_dir = ''; # default
+if($ARGV[0] eq "-bd")
 {
-	$test_type = splice(@ARGV,0,1);
+	$param_count++;
+	$build_dir = $ARGV[$param_count];
+	$param_count++;
 }
 
-my $mode = "Client";
-if($ARGV[0] eq "Client" || $ARGV[0] eq "Server")
+# specify the test type (Nightly, Experimantal, Continuous)
+my $test_type = "Nightly"; # default
+if($ARGV[$param_count] eq "Experimental" || $ARGV[$param_count] eq "Nightly" || $ARGV[$param_count] eq "Continuous")
 {
-	$mode = $ARGV[0];
+	$test_type = splice(@ARGV,$param_count,1);
 }
+
+# specify the mode (Client, Server)
+my $mode = "Client"; # default
+if($ARGV[$param_count] eq "Client" || $ARGV[$param_count] eq "Server")
+{
+	$mode = $ARGV[$param_count];
+	$param_count++;
+}
+
+# build the ug_dart_conf command
 my $command = "ug_dart_conf.pl";
 foreach $param (@ARGV)
 {
 	$command=join(' ',$command,$param);
 }
+
+# specify the architecture
+my $arch = $ARGV[$param_count];
+
+# specify the build parameter (COV, NOCOV)
+my $build_param = "NOCOV";
+if($arch =~ /GCOV/)
+{
+	$build_param = "COV";
+}
+
+# run the build- test-submit-cycle either client or open/close a Dashboard
+# on the server
+ 
 if($mode eq "Client")
 {
        # checkout only for the first build/test/submit cycle			
         #system("ug_dart_co $mode");
 	system("$command");
-	system("ug_dart_tests.pl");
-	system("ug_dart_client_test $test_type");
+	system("ug_dart_tests.pl $build_dir");
+	system("ug_dart_client_test $test_type $build_param $build_dir");
 }
 elsif($mode eq "Server")
 {
@@ -48,5 +83,5 @@ elsif($mode eq "Server")
        # generate the build tree only for the first build/test/submit cycle
         #system("ug_dart_tree");
 	system("$command");
-	system("ug_dart_server_start $test_type");
+	system("ug_dart_server_start $test_type $build_dir");
 }	
