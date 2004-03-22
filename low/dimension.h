d7f2c53d5cb8a6d212bf25a8713c1eb9fa35ec48
Thimo Neubauer <thimo@dune-project.org>
2004-03-22 11:38:25 +0000

    new mini-header to unify the dimension-handling
    [[Imported from SVN: r7572]]

---
+++ low/dimension.h
@@ -0,0 +1,38 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+/* $Id$ */
+
+/*
+
+   provide the usual preprocessor-defines for the dimension and complain
+   if no dimension was set
+
+ */
+
+
+#ifndef DIMENSION_H
+#define DIMENSION_H
+
+
+#ifdef _2
+#ifdef _3
+#error ****    define EITHER dimension _2 OR _3       ****
+#endif
+#define __TWODIM__
+#define DIM 2
+#define DIM_OF_BND 1
+#endif
+
+#ifdef _3
+#define __THREEDIM__
+#define DIM 3
+#define DIM_OF_BND 2
+#endif
+
+#ifndef _2
+#ifndef _3
+#error ****    define at least dimension two OR three        ****
+#endif
+#endif
+
+#endif
