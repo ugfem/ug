b4a3ca38730e16231930404788fbd720507ebd26
Oliver Sander <sander@dune-project.org>
2004-05-04 12:31:44 +0000

    doxygenized
    [[Imported from SVN: r7604]]

--- np/algebra/block.h
+++ np/algebra/block.h
@@ -4,16 +4,16 @@
 /*                                                                          */
 /* File:      block.h                                                       */
 /*                                                                          */
-/* Purpose:   block solver (header file)                                                    */
+/* Purpose:   block solver (header file)                                    */
 /*                                                                          */
-/* Author:	  Christian Wieners                                                                             */
-/*			  Institut fuer Computeranwendungen III                                                 */
-/*			  Universitaet Stuttgart										*/
-/*			  Pfaffenwaldring 27											*/
-/*			  70569 Stuttgart												*/
-/*			  email: ug@ica3.uni-stuttgart.de				                        */
-/*																			*/
-/* History:   Nov 27 95                                                                                 */
+/* Author:    Christian Wieners                                             */
+/*            Institut fuer Computeranwendungen III                         */
+/*            Universitaet Stuttgart                                        */
+/*            Pfaffenwaldring 27                                            */
+/*            70569 Stuttgart                                               */
+/* email:     ug@ica3.uni-stuttgart.de                                      */
+/*                                                                          */
+/* History:   Nov 27 95                                                     */
 /*                                                                          */
 /* Remarks:                                                                 */
 /*                                                                          */
@@ -66,16 +66,72 @@
 /*                                                                          */
 /****************************************************************************/
 
+
+/****************************************************************************/
+/** \brief Perform a matrix multiplication on the small blocks
+ *
+ * \param nr                    Number of rows
+ * \param nc                    Number of columns
+ * \param mcomp1                Components of matrix1
+ * \param mat1                  Array of matrix1
+ * \param mat2                  Array of matrix2
+ * \param resmat                Store mat1*mat2 here
+ *
+ * \return 0: ok  1: error
+ */
+/****************************************************************************/
 INT MatMulSmallBlock (SHORT nr, SHORT nc, SHORT n,
                       const SHORT *mcomp1, const DOUBLE *mat1,
                       const DOUBLE *mat2, DOUBLE *resmat);
+
+/****************************************************************************/
+/** \brief Invert a small system of equations
+ *
+ * \param n                     Size of the small system (n*n)
+ * \param mcomp                 Components of matrix to invert
+ * \param mat                   Array of this matrix
+ * \param invmat                Store inverse matrix here
+ *
+ * \return 0: ok  1: error
+ */
+/****************************************************************************/
 INT InvertSmallBlock (SHORT n, const SHORT *mcomp,
                       const DOUBLE *mat, DOUBLE *invmat);
+
+/****************************************************************************/
+/** \brief ???
+ *
+ * \param n                     Size of the small system (n*n)
+ * \param scomp                 Components of the solution
+ * \param sol                   Array of the solution
+ * \param invcomp               Components of inverse matrix
+ * \param inv                   Array of this matrix
+ * \param rhs                   Find right hand side here
+ *
+ * \return 0: ok  1: error
+ */
+/****************************************************************************/
 INT SolveInverseSmallBlock (SHORT n, const SHORT *scomp, DOUBLE *sol,
                             const SHORT *invcomp, const DOUBLE *inv,
                             const DOUBLE *rhs);
+
+/****************************************************************************/
+/** \brief Solve a small system of equations
+ *
+ * \param n                     Size of the small system (n*n)
+ * \param scomp                 Components of the solution
+ * \param sol                   Array of the solution
+ * \param mcomp                 Components of matrix to invert
+ * \param mat                   Array of this matrix
+ * \param rhs                   Find right hand side here
+ *
+ * \return 0: ok  1: error
+ */
+/****************************************************************************/
 INT SolveSmallBlock (SHORT n, const SHORT *scomp, DOUBLE *sol,
                      const SHORT *mcomp, const DOUBLE *mat, DOUBLE *rhs);
+
+
 INT InvertFullMatrix (INT n, DOUBLE mat[LOCAL_DIM][LOCAL_DIM],
                       DOUBLE invmat[LOCAL_DIM][LOCAL_DIM]);
 INT InvertSpdMatrix (INT n, DOUBLE mat[LOCAL_DIM][LOCAL_DIM],
