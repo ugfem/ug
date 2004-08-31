5f3dcf17945a86e08e78842f70f8b437003861da
Thimo Neubauer <thimo@dune-project.org>
2004-08-31 15:29:01 +0000

    dummy header-file so that using the autotools besides the UG build system is a bit more possible. If used autoconf will write a new config.h and arch/compiler.h can then decide to use the values autoconf estimated. Without it everything stays as it is.
    [[Imported from SVN: r7754]]

---
+++ config.h
@@ -0,0 +1,11 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+/* README:
+
+   This header is a dummy header so that both the classical UG build
+   system and a automake/autoconf-approach can be used. arch/compiler.h
+   switches it's functionality depending on symbols in config.h. As the
+   compiler complains if no config.h is found we need this dummy.
+
+   $Id$
+ */
