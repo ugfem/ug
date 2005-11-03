dc614acaa54156d82b98cfbeefd42bceec2ffb14
klaus <klaus>
2005-11-03 09:05:06 +0000

    new blocking strategy: 'ub' for ultimate blocking. Just the best, but under development
    [[Imported from SVN: r8115]]

--- np/procs/blocking.c
+++ np/procs/blocking.c
@@ -102,6 +102,10 @@ typedef struct
   INT r[MAXLEVEL];                                                      /* realized block size          */
 } NP_DD;
 
+typedef struct
+{
+  NP_BLOCKING blocking;
+} NP_UB;
 
 /****************************************************************************/
 /*																			*/
@@ -416,6 +420,7 @@ static INT DD_Blocking (NP_BLOCKING *theNP, GetMemProcPtr GetMem, INT level, MAT
       }
   }
   assert(i==n);
+  for (theV=FIRSTVECTOR(theGrid),i=0; theV!=NULL; theV=SUCCVC(theV)) vlist[i++]=theV;
   for (i=0; i<n; i++) GRID_UNLINK_VECTOR(theGrid,vlist[i]);
   for (i=0; i<n; i++) GRID_LINK_VECTOR(theGrid,vlist[i],PRIO(vlist[i]));
 
@@ -454,6 +459,149 @@ static INT DD_Construct (NP_BASE *theNP)
 }
 
 /****************************************************************************/
+/*D
+   ub - numproc for ub blocking on algebraic level
+
+   DESCRIPTION:
+   This numproc returns the (u)ltimate (b)locking
+
+   .vb
+   npinit <name>;
+   .ve
+
+   'npexecute not possible'
+
+   D*/
+/****************************************************************************/
+
+static INT UB_Init (NP_BASE *theNP, INT argc , char **argv)
+{
+  NP_UB *np=(NP_UB *)theNP;
+  return (NP_ACTIVE);
+}
+
+static INT UB_Display (NP_BASE *theNP)
+{
+  NP_UB *np=(NP_UB *)theNP;
+  return (0);
+}
+
+static INT UB_Block (MATRIX *m)
+{
+  INT n;
+  DOUBLE l,ll;
+  DOUBLE_VECTOR p1,p2,q;
+  VECTOR *v1,*v2;
+  MATRIX *theM;
+
+  v1=MDEST(m); v2=MDEST(MADJ(m));
+  VectorPosition(v1,p1); VectorPosition(v2,p2); V_DIM_EUKLIDNORM_OF_DIFF(p1,p2,l);
+  n=0;
+  for (theM=MNEXT(VSTART(v1)); theM!=NULL; theM=MNEXT(theM))
+  {
+    VectorPosition(MDEST(theM),q);
+    V_DIM_EUKLIDNORM_OF_DIFF(p1,q,ll);
+    if (ll>3.0*l) n++;
+  }
+  for (theM=MNEXT(VSTART(v2)); theM!=NULL; theM=MNEXT(theM))
+  {
+    VectorPosition(MDEST(theM),q);
+    V_DIM_EUKLIDNORM_OF_DIFF(p2,q,ll);
+    if (ll>3.0*l) n++;
+  }
+  return(n);
+}
+
+static INT UB_WeiredElem (ELEMENT *e)
+{
+  DOUBLE min,max;
+
+  min=3.14159265; max=0.0;
+  if (MinMaxAngle(e,&min,&max)) assert(0);
+  if (max>=0.666*3.14159265) return(1);
+  return(0);
+}
+
+static INT UB_Blocking (NP_BLOCKING *theNP, GetMemProcPtr GetMem, INT level, MATDATA_DESC *A, BLOCKING_STRUCTUR *bs, INT *result)
+{
+  NP_UB *np=(NP_UB *)theNP;
+  GRID *theGrid=NP_GRID(theNP,level);
+  void *buffer;
+  INT i,n,ne;
+  VECTOR **vlist, *theV;
+  MATRIX *theM;
+  FIFO fifo;
+  ELEMENT *e;
+
+  /* prepare */
+  n=NVEC(theGrid); ne=NT(theGrid);
+  vlist = (VECTOR**)(*GetMem)(sizeof(VECTOR*)*n); assert(vlist!=NULL);
+  bs->nb=(INT *)(*GetMem)(sizeof(INT)*(n+ne));
+  bs->vb=(VECTOR ***)(*GetMem)(sizeof(VECTOR**)*(n+ne));
+  buffer=(void *)(*GetMem)(sizeof(VECTOR*)*n); assert(buffer!=NULL);
+  fifo_init(&fifo,buffer,sizeof(VECTOR*)*n);
+  for (theV=FIRSTVECTOR(theGrid); theV!=NULL; theV=SUCCVC(theV)) SETVCUSED(theV,0);
+
+  /* get strongly coupled line, defined by UG_Block(theM)==0  */
+  for (bs->n=0; FIRSTVECTOR(theGrid)!=NULL; bs->n++)
+  {
+    fifo_in(&fifo,FIRSTVECTOR(theGrid)); SETVCUSED(FIRSTVECTOR(theGrid),1);
+    n=0;
+    while(!fifo_empty(&fifo))
+    {
+      theV=(VECTOR *)fifo_out(&fifo);
+      vlist[n++]=theV;
+      for (theM=MNEXT(VSTART(theV)); theM!=NULL; theM=MNEXT(theM))
+        if (UB_Block(theM) && !VCUSED(MDEST(theM)))
+        {
+          fifo_in(&fifo,(void *)MDEST(theM));
+          SETVCUSED(MDEST(theM),1);
+        }
+    }
+    bs->nb[bs->n]=n;
+    bs->vb[bs->n]=(VECTOR **)(*GetMem)(sizeof(VECTOR*)*n);
+    for (i=0; i<n; i++)
+    {
+      bs->vb[bs->n][i]=vlist[i];
+      GRID_UNLINK_VECTOR(theGrid,vlist[i]);
+    }
+  }
+  for (n=0; n<bs->n; n++)
+    for (i=0; i<bs->nb[n]; i++)
+      GRID_LINK_VECTOR(theGrid,bs->vb[n][i],PRIO(bs->vb[n][i]));
+
+  /* add vectors of weired elements */
+  for (e=FIRSTELEMENT(theGrid); e!=NULL; e=SUCCE(e))
+    if (UB_WeiredElem(e))
+    {
+      GetVectorsOfNodes(e,&n,vlist);
+      bs->nb[bs->n]=n;
+      bs->vb[bs->n]=(VECTOR **)(*GetMem)(sizeof(VECTOR*)*n);
+      for (i=0; i<n; i++)
+        bs->vb[bs->n][i]=vlist[i];
+      bs->n++;
+    }
+
+  return (0);
+}
+
+static INT UB_Construct (NP_BASE *theNP)
+{
+  NP_BLOCKING *np;
+
+  theNP->Init=UB_Init;
+  theNP->Display=UB_Display;
+  theNP->Execute=NULL;
+
+  np=(NP_BLOCKING *)theNP;
+  np->PreProcess=NULL;
+  np->Blocking=UB_Blocking;
+  np->PostProcess=NULL;
+
+  return(0);
+}
+
+/****************************************************************************/
 /*
    InitBlock	- Init this file
 
@@ -478,6 +626,7 @@ INT NS_DIM_PREFIX InitBlocking ()
   if (CreateClass(BLOCKING_CLASS_NAME ".elemblock",sizeof(NP_ELEM_BLOCK),ELEM_BLOCK_Construct)) REP_ERR_RETURN (__LINE__);
   if (CreateClass(BLOCKING_CLASS_NAME ".sab",sizeof(NP_SAB),SAB_Construct)) REP_ERR_RETURN (__LINE__);
   if (CreateClass(BLOCKING_CLASS_NAME ".dd",sizeof(NP_DD),DD_Construct)) REP_ERR_RETURN (__LINE__);
+  if (CreateClass(BLOCKING_CLASS_NAME ".ub",sizeof(NP_UB),UB_Construct)) REP_ERR_RETURN (__LINE__);
 
   return (0);
 }
