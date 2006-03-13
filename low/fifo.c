b534d8d2ba7cdadf5be6815b81d6f45952f06e26
Oliver Sander <sander@dune-project.org>
2006-03-13 09:04:00 +0000

    merged the tiny bit of information from fifo.doc into fifo.c and fifo.h
    [[Imported from SVN: r8152]]

--- low/fifo.c
+++ low/fifo.c
@@ -1,5 +1,9 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
+/** \file
+    \brief Functions to handle a fifo (first in, first out) structure
+ */
+
 /****************************************************************************/
 /*	                                                                        */
 /* File:      fifo.c                                                        */
--- low/fifo.h
+++ low/fifo.h
@@ -1,5 +1,9 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
+/** \file
+    \brief Header file for general purpose fifo (first in, first out) structure
+ */
+
 /****************************************************************************/
 /*                                                                          */
 /* File:      fifo.h                                                        */
