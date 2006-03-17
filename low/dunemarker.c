2eb1001d7478dbf279351820a5116316f483ef75
Oliver Sander <sander@dune-project.org>
2006-03-17 09:37:12 +0000

    These two files place an extra int in the ug library when ug is built with --enable-dune.  This allows the dune build system to check whether ug has been built with that option.  This will be helpful because the errors that result from forgetting --enable-dune are extremely strange.
    [[Imported from SVN: r8162]]

---
+++ low/dunemarker.c
@@ -0,0 +1,32 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+/****************************************************************************/
+/** \file
+    \brief Create a link time marker for the enable-dune build option.
+    \author Oliver Sander
+
+    If you want to use UG as part of DUNE, you have to build it with the
+    build-option --enable-dune.  However, this option affects header
+    files exclusively.  It is therefore impossible for the DUNE build
+    system to check whether UG has been built with this opion.  People
+    tend to forget it and then encounter strange errors.  To avoid this
+    we add a data field here which gets compiled into the UG library
+    if --enable-dune is set and can therefore be checked by the
+    DUNE build system.
+ */
+/****************************************************************************/
+
+
+/* RCS_ID
+   $Header$
+ */
+
+#include "namespace.h"
+#include "dunemarker.h"
+
+#ifdef FOR_DUNE
+/** \brief Data field which is only there for the DUNE build system to check
+    whether UG has been compiled with FOR_DUNE.
+ */
+int NS_PREFIX duneMarker = 0;
+#endif
